cb7ff690e0bc15babb93aa131ea3ac8bbc61dce3
Joel Terrell <falconreid@hotmail.com>
2020-11-09 09:56:01 -0700

    Updates

--- src/components/Logo/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./style.css";
-// import logo from "./EmpDirLogo.png";
-
-function Logo(props) {
-  return <img src={props.logo} alt={props.logo} />;
-}
-
-export default Logo;
